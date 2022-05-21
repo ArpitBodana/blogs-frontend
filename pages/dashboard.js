865188d5fbb16c2fd96fa7468876d3887d3048f7
ArpitBodana <arpitbodana2@gmail.com>
2022-05-21 15:26:46 +0530

    logo

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -127,7 +127,7 @@ export default function Dashboard({ blogs }) {
                         <TextField required defaultValue={blog.tag} label="Tag" onChange={(e) => setTag(e.target.value)} fullWidth={true} className='mt-3' />
                         <TextField value={blog.date} fullWidth={true} label="Posting Date" className='mt-3' />
 
-                        <div className='mt-4 flex justify-around border-b-2 border-blue-400'>
+                        <div className='mt-4 flex justify-around border-b-2 border-blue-400 mb-4'>
                             <Button variant='outlined' color='secondary' onClick={() => handleEdit(blog.id, blog.title, blog.body, blog.tag)} startIcon={<SaveIcon />}>Save</Button>
                             <Button variant='outlined' color='error' onClick={() => handleDelete(blog.id)} startIcon={<DeleteSharpIcon />}>Delete</Button>
                         </div>
